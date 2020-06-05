bb7894ddc9cf93e7e95602980940615a4e9ce3cb
Arom Jhee <aromjhee@gmail.com>
2020-06-05 16:08:20 -0700

    phase 9: add Add Park page, add sequelize model validation, render errors to webpage, use mixin

--- db/models/park.js
+++ db/models/park.js
@@ -4,30 +4,109 @@ module.exports = (sequelize, DataTypes) => {
     parkName: {
       type: DataTypes.STRING(255),
       allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please provide a value for Park Name',
+        },
+        notEmpty: {
+          msg: 'Please provide a value for Park Name',
+        },
+        len: {
+          args: [0, 255],
+          msg: 'Park Name must not be more than 255 characters long',
+        }
+      }
     },
     city: {
       type: DataTypes.STRING(100),
       allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please provide a value for City'
+        },
+        notEmpty: {
+          msg: 'Please provide a value for City'
+        },
+        len: {
+          args: [0, 100],
+          msg: 'City must not be more than 100 characters long'
+        }
+      }
     },
     provinceState: {
       type: DataTypes.STRING(100),
       allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please provide a value for Province/State'
+        },
+        notEmpty: {
+          msg: 'Please provide a value for Province/State'
+        },
+        len: {
+          args: [0, 100],
+          msg: 'Province/State must not be more than 100 characters long'
+        }
+      }
     },
     country: {
       type: DataTypes.STRING(100),
       allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please provide a value for Country'
+        },
+        notEmpty: {
+          msg: 'Please provide a value for Country'
+        },
+        len: {
+          args: [0, 100],
+          msg: 'Country must not be more than 100 characters long'
+        }
+      }
     },
     opened: {
       type: DataTypes.DATEONLY,
       allowNull: false,
+      validate: {
+        isDate: {
+          msg: 'Please provide a valid date for Opened'
+        },
+        notEmpty: {
+          msg: 'Please provide a value for Opened'
+        },
+        notNull: {
+          msg: 'Please provide a value for Opened'
+        }
+      }
     },
     size: {
       type: DataTypes.STRING(100),
       allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please provide a value for Size'
+        },
+        notEmpty: {
+          msg: 'Please provide a value for Size'
+        },
+        len: {
+          args: [0, 100],
+          msg: 'Size must not be more than 100 characters long'
+        }
+      }
     },
     description: {
       type: DataTypes.TEXT,
       allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Please provide a value for Description'
+        },
+        notNull: {
+          msg: 'Please provide a value for Description'
+        }
+      }
     }
   }, {});
   Park.associate = function(models) {
--- routes.js
+++ routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const csrf = require('csurf');
+
 const { environment } = require('./config');
 
 const db = require('./db/models');
 
 const router = express.Router();
-
+const csrfProtection = csrf({cookie: true});
 
 const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
 
@@ -27,6 +29,36 @@ router.get('/park/:id(\\d+)', asyncHandler( async(req, res) => {
   const park = await db.Park.findByPk(parkId);
 
   res.render('park-detail', { title: 'Park Detail', park });
+}));
+
+router.get('/park/add', csrfProtection, async(req, res) => {
+  const newPark = db.Park.build();
+  
+  res.render('park-add', { title: 'Add Park', newPark, csrfToken: req.csrfToken() });
+});
+
+router.post('/park/add', csrfProtection, asyncHandler( async(req, res) => {
+  const { parkName, city, provinceState, country, opened, size, description } = req.body;
+
+  const newPark = db.Park.build({
+    parkName, city, provinceState, country, opened, size, description
+  });
+
+  try {
+    await newPark.save();
+    res.redirect('/');
+  } catch(e) {
+    if (e.name === 'SequelizeValidationError') {
+      console.log('**************', e.errors.message)
+      const err = e.errors.map(error => error.message);
+      res.render('park-add', {
+        title: 'Add Park',
+        newPark,
+        err,
+        csrfToken: req.csrfToken()
+      })
+    } else next(e);
+  }
 }));
 
 
